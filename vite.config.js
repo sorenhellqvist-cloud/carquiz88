8a05935c40fd3577ec93b31c6caf6f684028d17c
Sören Hellqvist <soren.hellqvist@timede.se>
2026-01-06 08:32:19 +0100

    Configure base path for carquiz application
    Set base path for deployment on timede.se

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Detta gör att appen fungerar på timede.se/carquiz/
+  base: '/carquiz/', 
 })
