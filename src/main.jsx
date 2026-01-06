9acf3f9ae06993b700e1b6c43c15ea8f45b1bf95
Sören Hellqvist <soren.hellqvist@timede.se>
2026-01-06 10:04:42 +0100

    Remove missing index.css import to fix build errors

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 
 // Detta anropar React och ritar ut din App-komponent i 'root'-elementet
 ReactDOM.createRoot(document.getElementById('root')).render(
